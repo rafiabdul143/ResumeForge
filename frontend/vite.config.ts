f0fee5f5e59fa69381fe22db5e1ca3ab9043750a
Mohammed Rafi (Quadrant Technologies LLC) <mohdabdulrafi17@gmail.com>
2026-06-02 21:35:09 +0530

    lets try again

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../wwwroot',
+   outDir: 'dist',
     emptyOutDir: true, // cleans the wwwroot folder before building
   },
   server: {
